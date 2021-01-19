25c5a46975d1abe5409a8c99348cf90577981910
Uğur Kıroğlu <ugurkiroglu45101@hotmail.com>
2021-01-19 17:45:00 +0300

    Add project files.

---
+++ CustomerManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        
+        public void Welcome()
+        {
+            Console.WriteLine("Welcome to SonneBank Customer System!");
+        }
+        
+        public void Listeleme(Customer x)
+        {
+            Console.WriteLine(x.Age);
+            Console.WriteLine(x.Name);
+            Console.WriteLine(x.LastName);
+            Console.WriteLine(x.Sex);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sadece "listeleme" eklenmiştir.
+
+            Customer customer1 = new Customer();
+            Customer customer2 = new Customer();
+            CustomerManager customerManager = new CustomerManager();
+
+
+            customerManager.Welcome();
+           
+            //----------------------------------Müşteri Bilgileri----------------------------------\\
+            customer1.Id = 111111;
+            customer1.Age = 15;
+            customer1.Name = "Bora";
+            customer1.LastName = "Kıroğlu";
+            customer1.Sex = "Male";
+
+            customer2.Id = 111112;
+            customer2.Age = 19;
+            customer2.Name = "Uğur";
+            customer2.LastName = "Kıroğlu";
+            customer2.Sex = "Male";
+            //-------------------------------------------------------------------------------------\\
+            Customer[] customers = new Customer[] {customer1 , customer2};
+
+            foreach (var x in customers)
+            {
+                Console.WriteLine("--------------------");
+                customerManager.Listeleme(x);
+                Console.WriteLine("--------------------");
+            }
+        }
+    }
+}
